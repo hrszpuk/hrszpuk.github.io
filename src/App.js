761c69942828567fcb1fe443d010b7c2354bd851
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-08-28 21:13:32 +0100

    Added homepage stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Navbar from "./Navbar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+          <Navbar />
+          <h2>hrszpuk</h2>
+          <ul className="dashed">
+              <li>Hungry and ready for anything \( ﾟヮﾟ)/</li>
+              <br />
+              <li>Proficient in C/C++, Python, and Go!</li>
+              <li>Favourite foods: borscht, pasta bake, cider</li>
+              <li>(っ´ω`c)♡ coffee addict</li>
+              <li>Interested in programming language development, computer graphics, robotics, and hacking!</li>
+          </ul>
+          <br />
+          <h2>/blog</h2>
+          <ul className="dashed">
+              <li>Want to read about what I'm working on (╯°□°)╯???</li>
+              <br />
+              <li><a>0x00000000/</a> &lt;--- example</li>
+              <li><a>0x00000000/</a>&lt;--- stuff</li>
+          </ul>
+          <br />
+          <h2>Do you want to have some fun??!</h2>
+          <ul className="dashed">
+              <li>Check out the following links ٩(^‿^)۶</li>
+              <br />
+              <li><a>fun.hrszpuk.com</a> &lt;--- list of experiments/demos</li>
+          </ul>
+          <br />
+          <h2>/proj</h2>
+          <h4>Highlights</h4>
+          <h4>Notable projects</h4>
+          <ul className="dashed">
+              <li></li>
+          </ul>
+      </div>
+  )
 }
-
-export default App;
