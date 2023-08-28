10bfca94018f95380ede76ad3962252c9884aac2
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-08-28 19:17:59 +0100

    Gone... Reduced to atoms

--- netlify/functions/test.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event, context) {
-  const { identity, user } = context.clientContext;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "hello"
-    })
-  }
-};
