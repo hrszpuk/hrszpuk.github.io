a812ae3b12d8a8f8abd190fc2885f85a92991584
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-08-29 23:55:11 +0100

    Switched HTML to React elements

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import "./App.css"
 import Navbar from "./Navbar";
+import TitledList from "./TitledList";
+import BriefProjectInfo from "./briefProjectInfo";
 
 export default function App() {
   return (
       <div className="App">
           <Navbar />
-          <h2>hrszpuk</h2>
-          <ul className="dashed">
-              <li>Hungry and ready for anything \( ﾟヮﾟ)/</li>
-              <br />
-              <li>Proficient in C/C++, Python, and Go!</li>
-              <li>Favourite foods: borscht, pasta bake, cider</li>
-              <li>(っ´ω`c)♡ coffee addict</li>
-              <li>Interested in programming language development, computer graphics, robotics, and hacking!</li>
-          </ul>
-          <h2>/blog</h2>
-          <ul className="dashed">
-              <li>Want to read about what I'm working on (╯°□°)╯???</li>
-              <br />
-              <li><a>0x00000000/</a> &lt;--- example</li>
-              <li><a>0x00000000/</a>&lt;--- stuff</li>
-          </ul>
-          <h2>Do you want to have some fun??!</h2>
-          <ul className="dashed">
-              <li>Check out the following links ٩(^‿^)۶</li>
-              <br />
-              <li><a>fun.hrszpuk.com</a> &lt;--- list of experiments/demos</li>
-          </ul>
+          <TitledList
+              title="hrszpuk"
+              description="Hungry and ready for anything \( ﾟヮﾟ)/"
+              list={[
+                  "Proficient in C/C++, Python, and Go!",
+                  "Favourite foods: borscht, pasta bake, cider",
+                  "(っ´ω`c)♡ coffee addict",
+                  "Interested in programming language development, computer graphics, robotics, and hacking!",
+              ]}
+          />
+          <TitledList
+              title="/blog"
+              description="Want to read about what I'm working on (╯°□°)╯???"
+              list={[
+                  <p style={{display: "inline"}}><a>0x00000000/</a> &lt;--- example</p>,
+                  <p style={{display: "inline"}}><a>0x00000000/</a>&lt;--- stuff</p>,
+              ]}
+          />
+          <TitledList
+              title="Do you want to have some fun??!"
+              description="Check out the following links ٩(^‿^)۶"
+              list={[
+                  <p style={{display: "inline"}}><a>fun.hrszpuk.com</a> &lt;--- list of experiments/demos</p>,
+              ]}
+          />
           <h2>/proj</h2>
           <h4>> Highlights</h4>
           <h4>> Notable projects</h4>
           <ul className="dashed">
-              <li>(repo) <strong>title | brief description</strong><br />Longer description</li>
+              <BriefProjectInfo
+                  title="rectx"
+                  brief="A project manager for the ReCT programming language"
+                  description="a"
+              />
           </ul>
       </div>
   )
