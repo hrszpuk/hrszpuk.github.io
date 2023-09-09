6ee209010674413520261d1819dc1de69e0daec6
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-09-09 20:07:22 +0100

    Added react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./Error";
+import Blog from "./routes/Blog";
+import Proj from "./routes/Proj";
+import Fun from "./routes/Fun";
+import Post from "./routes/Post";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/blog",
+        element: <Blog />,
+        children: [
+            {
+                path: "/blog/:postId",
+                element: <Post />,
+            },
+        ],
+    },
+    {
+        path: "/proj",
+        element: <Proj />,
+    },
+    {
+        path: "/fun",
+        element: <Fun />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
